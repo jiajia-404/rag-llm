526950591a460cf3f191953010740b898714ca79
JiaJia <jiamuj04@gmail.com>
2024-07-28 23:47:15 +0100

    Code clean up

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -1,11 +1,7 @@
 from langchain_community.embeddings.ollama import OllamaEmbeddings
-from langchain_community.embeddings.bedrock import BedrockEmbeddings
 
 def get_embedding_function():
-    # embeddings = BedrockEmbeddings(
-    #     # credentials_profile_name="default", region_name="us-east-1"
-    #     credentials_profile_name="default", region_name="eu-west-2" 
-    # )
+
     embeddings = OllamaEmbeddings(model="llama3")
     return embeddings
 
